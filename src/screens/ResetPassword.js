302b588f1fb77f53082e74a541868ba45edc1803
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-12-01 00:19:45 +0700

    add form validation on reset password screen

--- src/screens/ResetPassword.js
+++ src/screens/ResetPassword.js
@@ -1,42 +1,82 @@
 import React from 'react';
 import {Image, StyleSheet, View} from 'react-native';
 import {Button, Text, Container, Content, Item, Input} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 import Logo from '../assets/img/logo-purple.png';
 
 export default function ResetPassword() {
+  const schema = Yup.object().shape({
+    password: Yup.string()
+      .min(6, 'Password required minimal 6 characters')
+      .required('Password field is required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], "Password doesn't match")
+      .required('Confirm password field is required'),
+  });
+
   return (
     <Container style={styles.parent}>
-      <Content style={styles.padding}>
-        <Image source={Logo} style={styles.logo} />
-        <Text style={styles.header}>Reset password</Text>
-        <Text style={styles.subHeader}>
-          You need to change your password to activate your account.
-        </Text>
-        <View>
-          <Text style={styles.label}>Kata sandi baru</Text>
-          <Item regular style={styles.fieldColor}>
-            <Input
-              placeholder="Masukkan kata sandi baru"
-              style={styles.input}
-              secureTextEntry
-            />
-          </Item>
-        </View>
-        <View style={styles.fieldMargin}>
-          <Text style={styles.label}>Konfirmasi kata sandi baru</Text>
-          <Item regular style={styles.fieldColor}>
-            <Input
-              placeholder="Masukkan konfirmasi kata sandi baru"
-              style={styles.input}
-              secureTextEntry
-            />
-          </Item>
-        </View>
-        <Button block style={styles.btnPrimary}>
-          <Text style={styles.textBtnPrimary}>Reset password</Text>
-        </Button>
-      </Content>
+      <Formik
+        initialValues={{
+          password: '',
+          confirmPassword: '',
+        }}
+        validationSchema={schema}
+        onSubmit={(values) => console.log(values)}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          touched,
+          errors,
+        }) => (
+          <Content style={styles.padding}>
+            <Image source={Logo} style={styles.logo} />
+            <Text style={styles.header}>Reset password</Text>
+            <Text style={styles.subHeader}>
+              You need to change your password to activate your account.
+            </Text>
+            <View>
+              <Text style={styles.label}>Kata sandi baru</Text>
+              <Item regular style={styles.fieldColor}>
+                <Input
+                  placeholder="Masukkan kata sandi baru"
+                  style={styles.input}
+                  secureTextEntry
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                />
+              </Item>
+              {touched.password && errors.password && (
+                <Text style={styles.error}>{errors.password}</Text>
+              )}
+            </View>
+            <View style={styles.fieldMargin}>
+              <Text style={styles.label}>Konfirmasi kata sandi baru</Text>
+              <Item regular style={styles.fieldColor}>
+                <Input
+                  placeholder="Masukkan konfirmasi kata sandi baru"
+                  style={styles.input}
+                  secureTextEntry
+                  onChangeText={handleChange('confirmPassword')}
+                  onBlur={handleBlur('confirmPassword')}
+                  value={values.confirmPassword}
+                />
+              </Item>
+              {touched.confirmPassword && errors.confirmPassword && (
+                <Text style={styles.error}>{errors.confirmPassword}</Text>
+              )}
+            </View>
+            <Button block style={styles.btnPrimary} onPress={handleSubmit}>
+              <Text style={styles.textBtnPrimary}>Reset password</Text>
+            </Button>
+          </Content>
+        )}
+      </Formik>
     </Container>
   );
 }
