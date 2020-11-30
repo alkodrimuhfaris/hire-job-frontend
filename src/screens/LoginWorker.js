23f7a4c709ce50fa6cb1208cb40d0db88f11f7be
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-30 23:07:18 +0700

    add form validation on login screen

--- src/screens/LoginWorker.js
+++ src/screens/LoginWorker.js
@@ -1,46 +1,89 @@
 import React from 'react';
 import {Image, TouchableOpacity, StyleSheet, View} from 'react-native';
 import {Button, Text, Container, Content, Item, Input} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 import Logo from '../assets/img/logo-purple.png';
 
 export default function LoginWorker() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Email is invalid')
+      .required('Email field is required'),
+    password: Yup.string()
+      .min(6, 'Password required minimal 6 characters')
+      .required('Password field is required'),
+  });
+
   return (
     <Container style={styles.parent}>
-      <Content style={styles.padding}>
-        <Image source={Logo} style={styles.logo} />
-        <Text style={styles.header}>Login</Text>
-        <View>
-          <Text style={styles.label}>Email</Text>
-          <Item regular style={styles.fieldColor}>
-            <Input placeholder="Masukkan alamat email" style={styles.input} />
-          </Item>
-        </View>
-        <View style={styles.fieldMargin}>
-          <Text style={styles.label}>Kata Sandi</Text>
-          <Item regular style={styles.fieldColor}>
-            <Input
-              placeholder="Masukkan kata sandi"
-              style={styles.input}
-              secureTextEntry
-            />
-          </Item>
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Lupa kata sandi</Text>
-        </TouchableOpacity>
-        <Button block style={styles.btnPrimary}>
-          <Text style={styles.textBtnPrimary}>Masuk</Text>
-        </Button>
-        <View style={styles.register}>
-          <Text style={styles.registerText}>Anda belum punya akun? </Text>
-          <TouchableOpacity>
-            <Text style={[styles.registerText, styles.registerLink]}>
-              Daftar disini
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Content>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={schema}
+        onSubmit={(values) => console.log(values)}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          touched,
+          errors,
+        }) => (
+          <Content style={styles.padding}>
+            <Image source={Logo} style={styles.logo} />
+            <Text style={styles.header}>Login</Text>
+            <View>
+              <Text style={styles.label}>Email</Text>
+              <Item regular style={styles.fieldColor}>
+                <Input
+                  placeholder="Masukkan alamat email"
+                  style={styles.input}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                />
+              </Item>
+              {touched.email && errors.email && (
+                <Text style={styles.error}>{errors.email}</Text>
+              )}
+            </View>
+            <View style={styles.fieldMargin}>
+              <Text style={styles.label}>Kata Sandi</Text>
+              <Item regular style={styles.fieldColor}>
+                <Input
+                  placeholder="Masukkan kata sandi"
+                  style={styles.input}
+                  secureTextEntry
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                />
+              </Item>
+              {touched.password && errors.password && (
+                <Text style={styles.error}>{errors.password}</Text>
+              )}
+            </View>
+            <TouchableOpacity>
+              <Text style={styles.forgot}>Lupa kata sandi?</Text>
+            </TouchableOpacity>
+            <Button block style={styles.btnPrimary} onPress={handleSubmit}>
+              <Text style={styles.textBtnPrimary}>Masuk</Text>
+            </Button>
+            <View style={styles.register}>
+              <Text style={styles.registerText}>Anda belum punya akun? </Text>
+              <TouchableOpacity>
+                <Text style={[styles.registerText, styles.registerLink]}>
+                  Daftar disini
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Content>
+        )}
+      </Formik>
     </Container>
   );
 }
@@ -78,7 +121,6 @@ const styles = StyleSheet.create({
     lineHeight: 19,
     fontWeight: '400',
     color: '#858D96',
-    fontFamily: 'open sans',
   },
   fieldColor: {
     backgroundColor: '#ffffff',
@@ -116,5 +158,9 @@ const styles = StyleSheet.create({
   },
   registerLink: {
     color: '#FBB017',
+  },
+  error: {
+    fontSize: 12,
+    color: 'red',
   },
 });
