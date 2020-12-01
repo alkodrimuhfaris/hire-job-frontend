af49ed1fd3abed3b3c4390437c14843608f3c920
M Faris Alkodri <farisalkodri@gmail.com>
2020-12-01 08:29:43 +0700

    linting

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,23 @@
-import {createStore, applyMiddleware} from 'redux'
-import logger from 'redux-logger'
-import promiseMiddleware from 'redux-promise-middleware'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import {persistStore, persistReducer} from 'redux-persist'
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import promiseMiddleware from 'redux-promise-middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
 
-import rootReducer from './reducers'
+import rootReducer from './reducers';
 
 const persistConfig = {
   key: 'root',
-  storage: AsyncStorage
-}
+  storage: AsyncStorage,
+};
 
-const persistedReducer =  persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
   const store = createStore(
     persistedReducer,
-    applyMiddleware( promiseMiddleware, logger )
-  )
-  const persistore = persistStore(store)
-  return {store, persistore}
-}
+    applyMiddleware(promiseMiddleware, logger),
+  );
+  const persistore = persistStore(store);
+  return {store, persistore};
+};
