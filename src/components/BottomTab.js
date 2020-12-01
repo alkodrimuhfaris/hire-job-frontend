c976a9cd2cc8115747528c899bf0799e2f067c6a
M Faris Alkodri <farisalkodri@gmail.com>
2020-12-01 15:09:37 +0700

    to development

--- src/components/BottomTab.js
+++ src/components/BottomTab.js
@@ -1,92 +1,100 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 //import assets icons
-import Home from '../assets/img/menu.svg'
-import HomeActive from '../assets/img/menuActive.svg'
-import Search from '../assets/img/search.svg'
-import SearchActive from '../assets/img/searchActive.svg'
-import Chat from '../assets/img/chat.svg'
-import ChatActive from '../assets/img/chatActive.svg'
-import Profile from '../assets/img/profile.svg'
-import ProfileActive from '../assets/img/profileActive.svg'
+import Home from '../assets/img/menu.svg';
+import HomeActive from '../assets/img/menuActive.svg';
+import Search from '../assets/img/search.svg';
+import SearchActive from '../assets/img/searchActive.svg';
+import Chat from '../assets/img/chat.svg';
+import ChatActive from '../assets/img/chatActive.svg';
+import Profile from '../assets/img/profile.svg';
+import ProfileActive from '../assets/img/profileActive.svg';
 
-const BottomNavigation = ({ state, descriptors, navigation }) => {
-    const focusedOptions = descriptors[state.routes[state.index].key].options;
+const BottomNavigation = ({state, descriptors, navigation}) => {
+  const focusedOptions = descriptors[state.routes[state.index].key].options;
 
-    if (focusedOptions.tabBarVisible === false) {
-        return null;
-    }
+  if (focusedOptions.tabBarVisible === false) {
+    return null;
+  }
 
-    return (
-        <View style={styles.container}>
-            {state.routes.map((route, index) => {
-                const { options } = descriptors[route.key];
-                const label =
-                    options.tabBarLabel !== undefined
-                        ? options.tabBarLabel
-                        : options.title !== undefined
-                            ? options.title
-                            : route.name;
+  return (
+    <View style={styles.container}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
 
-                const isFocused = state.index === index;
+        const isFocused = state.index === index;
 
-                const onPress = () => {
-                    const event = navigation.emit({
-                        type: 'tabPress',
-                        target: route.key,
-                        canPreventDefault: true,
-                    });
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
 
-                    if (!isFocused && !event.defaultPrevented) {
-                        navigation.navigate(route.name);
-                    }
-                };
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
 
-                const onLongPress = () => {
-                    navigation.emit({
-                        type: 'tabLongPress',
-                        target: route.key,
-                    });
-                };
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
 
-                const Icon = () => {
-                    if (label === "Home") return isFocused ? <HomeActive /> : <Home />
-                    if (label === "Search") return isFocused ? <SearchActive /> : <Search />
-                    if (label === "Chat") return isFocused ? <ChatActive /> : <Chat />
-                    if (label === "Profile") return isFocused ? <ProfileActive /> : <Profile />
-                    return <Home />
-                }
+        const Icon = () => {
+          if (label === 'Home') {
+            return isFocused ? <HomeActive /> : <Home />;
+          }
+          if (label === 'Search') {
+            return isFocused ? <SearchActive /> : <Search />;
+          }
+          if (label === 'Chat') {
+            return isFocused ? <ChatActive /> : <Chat />;
+          }
+          if (label === 'Profile') {
+            return isFocused ? <ProfileActive /> : <Profile />;
+          }
+          return <Home />;
+        };
 
-                return (
-                    <TouchableOpacity
-                        key={index}
-                        onPress={onPress}
-                        onLongPress={onLongPress}
-                        style={styles.iconTab}>
-                        <Icon />
-                    </TouchableOpacity>
-                );
-            })}
-        </View>
-    );
-}
+        return (
+          <TouchableOpacity
+            key={index}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={styles.iconTab}>
+            <Icon />
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
 
-export default BottomNavigation
+export default BottomNavigation;
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        backgroundColor: '#FFFFFF',
-        justifyContent: 'space-between',
-        paddingHorizontal: 25,
-        paddingVertical: 13,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        height: 70,
-    },
-    iconTab:{
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'space-between',
+    paddingHorizontal: 25,
+    paddingVertical: 13,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height: 70,
+  },
+  iconTab: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const Search = () => {
-    return (
-        <View style={styles.parent}>
-            <Text>Search Screen</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.parent}>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
 
-export default Search
+export default Search;
 
 const styles = StyleSheet.create({
-    parent:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  parent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
