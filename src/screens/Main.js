175935f01b6cbf891de70663a603c9f96643e19e
M Faris Alkodri <farisalkodri@gmail.com>
2020-12-01 08:26:31 +0700

    fix linter

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 // import screens
 import Welcome from './Welcome';
 import LoginWorker from './LoginWorker';
 import LoginRecruiter from './LoginRecruiter';
 import SignupRecruiter from './SignupRecruiter';
-import SignupWorker from './SignupWorker'
+import SignupWorker from './SignupWorker';
 import Forgot from './ForgotPassword';
 import Reset from './ResetPassword';
-import Notif from './Notification'; 
+import Notif from './Notification';
 
 // import navigator
 const Stack = createStackNavigator();
@@ -25,7 +25,7 @@ export default function Main() {
           <Stack.Screen
             name="Welcome"
             component={Welcome}
-            options={{ headerShown: false }}
+            options={{headerShown: false}}
           />
           <Stack.Screen
             name="LoginWorker"
@@ -55,12 +55,12 @@ export default function Main() {
           <Stack.Screen
             name="SignupWorker"
             component={SignupWorker}
-            options={{ headerShown: false }}
+            options={{headerShown: false}}
           />
           <Stack.Screen
             name="Notif"
             component={Notif}
-            options={{ headerShown: false }}
+            options={{headerShown: false}}
           />
         </Stack.Navigator>
       )}
